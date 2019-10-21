42dd2bc7c31c21dd2382f9bacf7f547d94f8efc3
Xin-Chen <xinchen1997@yahoo.com>
2019-10-20 21:34:36 -0700

    finished

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -27,7 +27,7 @@ Scenario: restrict to movies with 'PG' or 'R' ratings
   Given I check the following ratings: PG, R
 
   # enter step(s) to uncheck all other checkboxes
-  And I uncheck the following ratings: G, PG-17, NC-17
+  And I uncheck the following ratings: G, PG-13, NC-17
 
   # enter step to "submit" the search form on the homepage
   And I press "Refresh"
@@ -52,3 +52,6 @@ Scenario: restrict to movies with 'PG' or 'R' ratings
 
 Scenario: all ratings selected
   # see assignment
+  When I check the following ratings: PG, R, G, PG-13, NC-17
+  And I press "Refresh"
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -13,13 +13,28 @@ Then /(.*) seed movies should exist/ do | n_seeds |
   Movie.count.should be n_seeds.to_i
 end
 
+Then /I should see the following movies/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    expect(page).to have_content(movie['title'])
+  end
+end
+
+Then /I should not see the following movies/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    expect(page).to_not have_content(movie['title'])
+  end
+end
+
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  p1 = page.body.index(e1)
+  p2 = page.body.index(e2)
+  expect(e1).to be <= p2
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -30,10 +45,14 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(', ').each do |rating|
+    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+   expect(find_by_id('movies').all('tr').size - 1).to eq 10
+   #fail "Unimplemented"
 end
